8a089c0fa473538d73c929f8783389b151f85e2d
benny.hsieh <hotman0901@gmail.com>
2018-11-08 09:51:11 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,90 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UseStateHook from './components/useStateHook';
+import UseEffectHook from './components/useEffectHook';
+import Count from './components/count';
+import ContextParent from './components/contextParent';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            template: 'contextApi'
+        };
+    }
+
+    changeTemplate = (template) => {
+        this.setState(() => ({
+            template
+        }));
+    };
+
+    renderTemplate() {
+        const { template } = this.state;
+
+        switch (template) {
+            case 'useState':
+                return (
+                    <div>
+                        <h2>useState</h2>
+                        <UseStateHook />
+                        <hr />
+                        <Count />
+                    </div>
+                );
+            case 'useEffect':
+                return (
+                    <div>
+                        <UseEffectHook />
+                    </div>
+                );
+            case 'contextApi':
+                return (
+                    <div>
+                        <ContextParent />
+                    </div>
+                );
+            default:
+                break;
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <div className="menuList">
+                        <div
+                            className="menu"
+                            onClick={() => {
+                                this.changeTemplate('useState');
+                            }}
+                        >
+                            useState
+                        </div>
+                        <div
+                            className="menu"
+                            onClick={() => {
+                                this.changeTemplate('useEffect');
+                            }}
+                        >
+                            useEffect
+                        </div>
+                        <div
+                            className="menu"
+                            onClick={() => {
+                                this.changeTemplate('contextApi');
+                            }}
+                        >
+                            contextApi
+                        </div>
+                    </div>
+                    {this.renderTemplate()}
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
